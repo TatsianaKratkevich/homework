5d96e8e5cb07f1c7a83316b6f900567683feff2c
TatsianaKratkevich <lina.prokopeva.02@mail.ru>
2023-09-16 19:45:28 +0300

    homework1

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,33 @@
+type AddressType = {
+  street: string; // ПОДПРАВЛЯЕМ any
+  city: string; // ПОДПРАВЛЯЕМ any
+};
+
+type UserType = {
+  id:number;
+  name:string;
+  age:number;
+  address:AddressType;
+  // ПРИДЕТСЯ САМОМУ)
+};
+
+type UserListPropsType = {
+  users: Array<UserType> // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
+
+export const UserList = (props: UserListPropsType) => {
+  return (
+    <div id={'hw01-users'}>
+      <h2>User List:</h2>
+
+      <ul>
+       {props.users.map((el) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+         <li key={el.id} id={`hw01-user-${el.id}`}>
+          <strong>{el.name}</strong> (Age: {el.age})<strong> Address:</strong>
+            {el.address.street}, {el.address.city}
+         </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
